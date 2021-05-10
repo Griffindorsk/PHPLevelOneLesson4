3386c6f3c4580f3cef8f7894d785b9271fc45ba1
Griffindorsk <78537150+Griffindorsk@users.noreply.github.com>
2021-05-10 18:31:08 +0300

    just to create formal PullRequest

--- templates/layout.php
+++ templates/layout.php
@@ -6,11 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$title?></title>
     <link rel="stylesheet" type="text/css" href="styles/style.css"/>
-    <!-- <script type="text/javascript" src="scripts/jquery-1.4.3.min.js"></script>
-    <script type="text/javascript" src="scripts/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-    <script type="text/javascript" src="scripts/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-    <link rel="stylesheet" type="text/css" href="scripts/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
-    <script src="scripts/add.js"></script> -->
 </head>
 <body>
 <?=$content?>
